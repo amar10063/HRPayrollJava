0c5a45c4f2509898b83619a0ca9e7de3454d6658
amar10063 <amar10063@yahoo.co.in>
2019-03-11 17:38:38 +0530

    min lenght added in password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,8 +13,8 @@ export class LoginComponent implements OnInit {
   submitted = false;
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
   onSubmit() {
