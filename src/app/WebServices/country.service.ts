051c25e274116c1cff0556848030eceaf4dcfa10
AvantikaTyagi4 <tyagiavantika2@gmail.com>
2019-04-05 10:15:30 +0530

    call service in contry

--- src/app/WebServices/country.service.ts
+++ src/app/WebServices/country.service.ts
@@ -2,8 +2,9 @@ import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Urls } from './Urls';
 import { Injectable } from '@angular/core';
-import { CountryBody } from '../SystemAdministration/country/CountryBody';
-
+import { CountryBody } from '../SystemAdministration/country/CountryDetails/CountryBody';
+import { CityBody } from '../SystemAdministration/country/CityDetails/CityBody';
+import { CountryResponse } from '../SystemAdministration/country/CountryDetails/CountryResponse';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,9 +16,34 @@ export class CountryService {
       'Content-Type': 'application/json'
     })
   };
+
+
   doLogin(countryBody: CountryBody): Observable<any> {
     return this.httpClient.post<any>(Urls.baseUrls + Urls.saveCountry, JSON.stringify(countryBody), this.httpOptions);
   }
+  saveCity(cityBody: CityBody): Observable<any> {
+    return this.httpClient.post<any>(Urls.baseUrls + Urls.saveCity, JSON.stringify(cityBody), this.httpOptions);
+  }
+  
+  // getCountries(countryBody: CountryBody[]): Observable<any>
+  // {
+  //   return this.httpClient.post<any>(Urls.baseUrls + Urls.getCountry, JSON.stringify(countryBody), this.httpOptions);
+  // }
+  // countries(countryBody: CountryBody): Observable<any> {
+  //   return this.httpClient.post<any>(Urls.baseUrls + Urls.getCountry, JSON.stringify(countryBody), this.httpOptions);
+  // }
+  // countries(): Observable<CountryResponse[]> {
+  //   return this.httpClient.post<any>(Urls.baseUrls + Urls.getCountry,
+  //     .map((response: Response) => response.json())
+  //                   );
+  // }
+
+
+
+  //   cellEditorParams: {
+  //     values: countries,
+  //     cellRenderer: (params) => params.value.name
+  // }
 }
 
 
