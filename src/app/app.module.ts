0efeb7986360e0b1b8d9fb92e8e1c203e252e542
aarati_vashisht <aarati.vashisht2804@gmail.com>
2019-04-12 15:47:19 +0530

    App Modeule Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,8 +19,6 @@ import { CountryComponent } from './SystemAdministration/country/country.compone
 import { SetupComponent } from './HRPayroll/setup/setup.component';
 import { GlobalSearchComponent } from './global-search/global-search.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { BasicdetailsserviceService } from './webservices/basicdetailsservice.service';
 import { XhrInterceptor } from './XhrInterceptor';
 import { OrganizationComponent } from './SystemAdministration/organization/organization.component';
 import { PayrollComponent } from './SystemAdministration/payroll/payroll.component';
@@ -64,7 +62,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     HttpClientModule
 
   ],
-  providers: [BasicdetailsserviceService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }], entryComponents: [LocationDropdownComponent],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }], entryComponents: [LocationDropdownComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
